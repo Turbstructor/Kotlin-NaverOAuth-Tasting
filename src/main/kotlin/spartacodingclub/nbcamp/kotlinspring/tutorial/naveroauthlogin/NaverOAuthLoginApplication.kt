ecaed726b15ee469ef6c61b9e0c883f7150a378f
Turbstructor <ycw0201@naver.com>
2024-06-04 18:24:16 +0900

    Initial commit

---
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/tutorial/naveroauthlogin/NaverOAuthLoginApplication.kt
@@ -0,0 +1,11 @@
+package spartacodingclub.nbcamp.kotlinspring.tutorial.naveroauthlogin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NaverOAuthLoginApplication
+
+fun main(args: Array<String>) {
+	runApplication<NaverOAuthLoginApplication>(*args)
+}
---
+++ src/test/kotlin/spartacodingclub/nbcamp/kotlinspring/tutorial/naveroauthlogin/NaverOAuthLoginApplicationTests.kt
@@ -0,0 +1,13 @@
+package spartacodingclub.nbcamp.kotlinspring.tutorial.naveroauthlogin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NaverOAuthLoginApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
