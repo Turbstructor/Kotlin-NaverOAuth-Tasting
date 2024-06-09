6646a58c2447dee86fa215052daf2a6803e4c58c
Turbstructor <ycw0201@naver.com>
2024-06-09 13:55:39 +0900

    💩 Feat: add Naver OAuth authorization and info extraction
    Implement Naver OAuth login and info extraction from social account, but AuthController need improvement with less bond with Naver OAuth(its logic should be implemented elsewhere)

--- src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/tutorial/naveroauthlogin/domain/auth/dto/response/NaverLoginAuthResponse.kt
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/tutorial/naveroauthlogin/domain/auth/dto/response/NaverLoginAuthResponse.kt
@@ -1,8 +1,12 @@
 package spartacodingclub.nbcamp.kotlinspring.tutorial.naveroauthlogin.domain.auth.dto.response
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class NaverLoginAuthResponse(
-    val code: String,
-    val state: String,
-    val error: String,
-    val errorDescription: String
+    val accessToken: String = "",
+    val refreshToken: String = "",
+    val tokenType: String = "",
+    val expiresIn: Int = 0
 )
